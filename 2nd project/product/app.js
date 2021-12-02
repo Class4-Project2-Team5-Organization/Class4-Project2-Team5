7582ca7277faab9b616231e744bf32fa1d78df6c
JONG CHAN <92711587+dal0110@users.noreply.github.com>
2021-12-02 11:17:16 +0900

    productlist

---
+++ 2nd project/product/app.js
@@ -0,0 +1,16 @@
+const exprss = require("express");
+const app = exprss();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(exprss.static("views"));
+const host = '127.0.0.1';
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.render('indexpug');
+}); 
+
+app.listen(port, host, ()=>{
+    console.log(`Index appliction running at http://${host}:${port}/`);
+});
