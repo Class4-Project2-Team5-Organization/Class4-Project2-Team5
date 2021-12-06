2b39b7a8d547027bf80b8a4aea5fdce4bd2af759
jhin93 <scene1993@gmail.com>
2021-12-06 16:44:24 +0900

    두개의 디비 혹은 두개의 테이블을 연결해서 사용. constraint 로 검색 시도할 것.

--- 2nd project/LogReg/index.js
+++ 2nd project/LogReg/index.js
@@ -2,7 +2,17 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const routes = require('./routes');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
 const app = express();
+const mysql = require('mysql');
+
+const dbConnection = mysql.createPool({
+    host: 'localhost', // 사용할 host로 변경
+    user: 'root', // 본인이 사용할 user로 변경
+    password: 'wlsrud20', // 본인 mysql 접속 비밀번호으로 변경해서 저장할 것.
+    database: 'testlogreg' 
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -25,5 +35,69 @@ app.use((err, req, res, next) => {
     // console.log(err);
     return res.send('Internal Server Error');
 });
+
+
+// ------게시판 로직--------
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.get('/review', (req,res)=>{
+    let sql = "select * from testmerge";
+    let query = dbConnection.query(sql,(err,rows)=>{ 
+        if(err) throw err;
+        res.render("user_index", {
+            title: "서비스 후기게시판입니다.",
+            users : rows
+        });
+    });
+});
+
+app.get('/review_add',(req,res)=>{
+    res.render("user_add", {
+        title: "게시글 작성",
+    });
+})
+
+app.post('/save',(req,res)=>{
+    let data = {title : req.body.title, message : req.body.message};
+    let sql = "insert into testmerge SET ?";
+    let query = dbConnection.query(sql, data,(err, results)=>{
+        if(err) throw err;
+        res.redirect('/review');
+    });
+});
+
+app.get('/edit/:userid',(req,res)=>{
+    const userId = req.params.userid;
+    let sql = `select *from testmerge where id=${userId}`;
+    let query = dbConnection.query(sql, (err,result)=>{
+        if(err) throw err;
+        res.render('user_edit',{
+            title:'게시글 편집',
+            user : result[0]
+        });
+    });
+});
+
+app.post('/update', (req, res) => {
+    const userId = req.body.id;
+    let sql = "update testmerge SET title='" + req.body.title +"', message='" + req.body.message +"' where id="+userId;
+    let query = dbConnection.query(sql, (err, results) => {
+        if (err) throw err;
+        res.redirect('/review');
+    });
+});
+
+app.get('/delete/:userid', (req, res) => {
+    const userId = req.params.userid;
+    let sql = `DELETE from testmerge where id=${userId}`;
+    let query = dbConnection.query(sql, (err, result) => {
+        if (err) throw err;
+        res.redirect('/review');
+    });
+});
+
 
 app.listen(3000, () => console.log('Server is runngin on port http://localhost:3000'));
--- 2nd project/LogReg/utils/dbConnection.js
+++ 2nd project/LogReg/utils/dbConnection.js
@@ -4,7 +4,7 @@ const dbConnection = mysql.createPool({
     host: 'localhost', // 사용할 host로 변경
     user: 'root', // 본인이 사용할 user로 변경
     password: 'wlsrud20', // 본인 mysql 접속 비밀번호으로 변경해서 저장할 것.
-    database: 'orthodox_logreg' 
+    database: 'testlogreg' 
 });
 
 module.exports = dbConnection.promise();
