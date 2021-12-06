e79646461fd1ecbf9466a40055b4a5a940b38523
WONHYUN KWON <mywonhyuni@naver.com>
2021-12-06 19:20:17 +0900

    211206_1919_whkwon
    첫 번째 commit
    시은이 DB본다고해서 현 시간 기준 미완성본 올림

---
+++ Project2-whkwon/routes/routes.js
@@ -0,0 +1,23 @@
+// 마이페이지에서 실행 될 함수들 부르는 url 입력 시(아마 controller?) "실행 될 함수 이름만 적기"
+// ★여긴 단순하게 "어떤 함수를 실행할 건지만 간단하게 적고, 그 함수를 정의(실행)가 어떻게 되는지 자세한 코드는 controllers.js에서 실행"
+// 즉, 단순히 불러온다 라는 개념에서 routes.js를 쓰고 불러서 뭐 할꺼냐? 라는 함수 정의는 controllers.js에서 할 것
+
+const express = require("express");
+var router = express.Router();
+var controller = require("../controllers/controllers.js");
+require("../models/models");
+
+
+// ★핵심: ("경로", "미들웨어 함수") 만 적는다. 미들웨어는 controllers에서 관리
+// GET
+router.get('/mypage', controller.rendermypage);
+router.get('/modify', controller.rendermypageModify);
+router.get('/qna', controller.rendermypageQna);
+router.get('/qnalist', controller.rendermypageQnaList);
+
+// POST
+router.post('/mypage', controller.rendermypageButton);
+router.post('/modify', controller.rendermypageModify2);
+router.post('/qna', controller.rendermypageQnaPost);
+
+module.exports = router;
