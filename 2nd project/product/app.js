b79b87828579785496429c18d74c8f682884e704
dal0110 <92711587+dal0110@users.noreply.github.com>
2021-12-06 23:53:35 +0900

    product renewal

--- 2nd project/product/app.js
+++ 2nd project/product/app.js
@@ -1,16 +1,54 @@
-const exprss = require("express");
-const app = exprss();
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const testRouter = require("./router/index");
+const mysql = require("./dbcon");
 
-app.set('view engine', 'pug');
-app.set('views', './views');
-app.use(exprss.static("views"));
-const host = '127.0.0.1';
+const app = express();
+const host = "127.0.0.1";
 const port = 3000;
+app.use("/index", testRouter);
 
-app.get('/', (req, res) => {
-    res.render('indexpug');
-}); 
+app.set('view engine', 'ejs');
+app.set('views', __dirname+'/views');
 
-app.listen(port, host, ()=>{
-    console.log(`Index appliction running at http://${host}:${port}/`);
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use("/public", express.static('public'));
+app.use('/css',express.static(__dirname+'/assets/css/main.css'));
+app.use('/js',express.static(__dirname+'/views/function.js'));
+
+app.get("/product_list", (req, res) => {
+    const sql = `select * from product`;
+    try {
+        mysql.getConnection((err, connection) => {
+            console.log("connection_pool GET");
+            if(err) throw err;
+            connection.query(sql, (err, result, fields) => {
+                if(err) {
+                    console.error("connection_pool GET Error / " + err);
+                    res.status(500).send("message : Internal Server Error");
+                } else {
+                    if(result.length === 0){
+                        res.status(400).send({
+                            success : false,
+                            message : "DB response Not Found"
+                        })
+                    } else {
+                        res.render('ProductList', {product : result});
+                    };
+                };
+            });
+            connection.release();
+        })
+    } catch (err) {
+        console.error("connection_pool GET Error / " + err);
+        res.status(500).send("message : Internal Server Error");
+        connection.release(); // 에러를 받아서 반납을 해준다
+    }
+});
+
+
+app.listen(port, host, () => {
+    console.log(`Application server running at http://${host}:${port}/`);
 });
---
+++ 2nd project/product/router/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const path = require('path');
+const mysql = require('../dbcon');
+
+// router.get('/product_list', (req, res) => {
+//   let sql = `select * from product` 
+//   con.query(sql, function(err, result, fields) {
+//       if(err) throw err;
+//       res.render('ProductList', {product : result});
+//   });
+//   connection.release(); // 추가 
+// });
+
+// router.get('/product_detail/:id', (req, res) => {
+//   let sql = `select * from product
+//   where name = '면도세트 12개입'`
+//   con.query(sql, function(err, result, fields) {
+//       if(err) throw err;
+//       res.render('ProductDetail', {product : result});
+//   });
+//   connection.release(); // 추가 
+// });
+
+router.post("/cart", (req, res) => {
+  const sql = `INSERT INTO cart (user_name, product_id, quantity) VALUES ('test', '1', '1')`;
+  mysql.getConnection((err, connection) => {
+      if(err) throw err;
+      connection.query(sql, (err, result) =>{
+          if(err) throw err;
+          console.log(result);
+      });
+      connection.release();
+  });
+  res.redirect("/")
+
+});
+
+
+module.exports = router;
