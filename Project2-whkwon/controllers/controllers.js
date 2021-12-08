56b54cfe9254c0190122c3c537f747abbed87965
WONHYUN KWON <mywonhyuni@naver.com>
2021-12-08 11:03:59 +0900

    211208_1101_whkwon
    화이팅

--- Project2-whkwon/controllers/controllers.js
+++ Project2-whkwon/controllers/controllers.js
@@ -19,51 +19,81 @@ const express = require("express");
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // ★핵심: 요청을 받았을 때 어떤 "미들웨어" 실행할래? => 해당 미들웨어 함수를 routes.js에서 실행
 // Read - My Page
 exports.rendermypage = (req, res) => {
-    models.readMypage().then((result) => {
-        res.render("result-myPage", {userName: result[2].name, userEmail: result[2].email, userAddr: result[2].addr});
+  models.readMypage().then((result) => {
+    res.render("result-myPage", {
+      userName: result[2].name,
+      userEmail: result[2].email,
+      userAddr: result[2].addr,
     });
+  });
 };
 
 // Before Update - infoModify
-exports.rendermypageModify = (req, res) => { // input value=변수 => 벡틱+$ => `${변수}`
-    models.readMypage().then((result) => {
-        res.render("infoModify", {username: result[2].name, useremail: result[2].email, useraddr: result[2].addr});
-
+exports.rendermypageModify = (req, res) => {
+  // input value=변수 => 벡틱+$ => `${변수}`
+  models.readMypage().then((result) => {
+    res.render("infoModify", {
+      username: result[2].name,
+      useremail: result[2].email,
+      useraddr: result[2].addr,
     });
+  });
 };
 
 // After Update - My page(patch)
 exports.rendermypageButton = (req, res) => {
-    // ★★ PK/FK 구현 후 해당 값으로 바꿔야 함 ★★ or 각자 테이블 쓸꺼면 이대로 가도 될 듯
-    exports.modifyInfo = {name: req.body.modiname, email: req.body.modiemail, addr: req.body.modiaddr};
-    // exports.modifyInfo = "UPDATE mypage_test1 SET email = '1234556' where name = 'testName';";
-    console.log(req.body);
-    models.updateMypage().then(() => {
-        res.redirect("http://localhost:3000/mypage");
-    });
+  // ★★ PK/FK 구현 후 해당 값으로 바꿔야 함 ★★ or 각자 테이블 쓸꺼면 이대로 가도 될 듯
+  exports.modifyInfo = {
+    name: req.body.modiname,
+    email: req.body.modiemail,
+    addr: req.body.modiaddr,
+  };
+  // exports.modifyInfo = "UPDATE mypage_test1 SET email = '1234556' where name = 'testName';";
+  console.log(req.body);
+  models.updateMypage().then(() => {
+    res.redirect("http://localhost:3000/mypage");
+  });
 };
 
-
-
+// Order Page
+exports.renderOrderpage = (req, res) => {
+  models.renderSubs().then(() => {
+    res.render("myOrder", {
+      subsName: result[0].subsname,
+      subsDetail: result[0].subsdetail,
+      subsDate: result[0].subsdate,
+      subsPeriod: result[0].subsperiod,
+    });
+    console.log("success");
+  });
+  models.renderProd().then(() => {
+    res.render("myOrder", {
+      prodName: result[0].prodname,
+      prodCate: result[0].prodcate,
+      prodPrice: result[0].prodprice,
+      prodDate: result[0].proddate,
+    });
+  });
+};
 
 
 exports.rendermypageModify2 = (req, res) => {
-    res.render("result-myPage");
+  res.render("result-myPage");
 };
 
 exports.rendermypageQna = (req, res) => {
-    res.render("qna");
+  res.render("qna");
 };
 
 exports.rendermypageQnaPost = (req, res) => {
-    res.render("qnaList");
+  res.render("qnaList");
 };
 
 exports.rendermypageQnaList = (req, res) => {
-    res.render("qnaList");
+  res.render("qnaList");
 };
