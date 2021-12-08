62183544ec849d30ff3e80c70d357f377d5c17f0
jhin93 <scene1993@gmail.com>
2021-12-08 11:11:12 +0900

    게시판 배제 완료

--- 2nd project/Review/app.js
+++ 2nd project/Review/app.js
@@ -5,30 +5,21 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const app = express();
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'wlsrud20', //본인 root계정 비밀번호작성
-    database: 'orthodox_review'
-}); 
+// ------게시판 로직--------
 
-connection.connect( (err) => {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
+const dbConnection = mysql.createPool({
+    host: 'localhost', // 사용할 host로 변경
+    user: 'root', // 본인이 사용할 user로 변경
+    password: 'wlsrud20', // 본인 mysql 접속 비밀번호으로 변경해서 저장할 것.
+    database: 'testmerge' 
 });
 
-app.set('views', path.join(__dirname,'views'));
-app.set('view engine','ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.get('/', (req,res)=>{
-    let sql = "select * from users";
-    let query = connection.query(sql,(err,rows)=>{ 
+app.get('/review', (req,res)=>{
+    let sql = "select * from reviewboard";
+    let query = dbConnection.query(sql, (err,rows)=>{ 
         if(err) throw err;
         res.render("user_index", {
             title: "서비스 후기게시판입니다.",
@@ -37,25 +28,32 @@ app.get('/', (req,res)=>{
     });
 });
 
-app.get('/add',(req,res)=>{
+app.get('/review_add',(req,res)=>{
+    // users 테이블의 name을 reviewboard의 name으로 대입
     res.render("user_add", {
         title: "게시글 작성",
     });
 })
 
 app.post('/save',(req,res)=>{
-    let data = {name : req.body.name, title : req.body.title, message : req.body.message};
-    let sql = "insert into users SET ?";
-    let query = connection.query(sql, data,(err, results)=>{
+    let data = {name: req.body.name, title : req.body.title, message : req.body.message};
+    let sql = "insert into reviewboard SET ?";
+    let query = dbConnection.query(sql, data,(err, results)=>{
         if(err) throw err;
-        res.redirect('/');
+        res.redirect('/review');
     });
+    // join으로 reviewboard 테이블에 user 테이블의 name 넣기
+    // let namesql = 'insert into reviewboard (name) select (name) from users';
+    // let namequery = dbConnection.query(namesql, (err,results)=>{
+    //     if(err) throw err;
+    //     console.log("reviewboard 확인");
+    // })
 });
 
 app.get('/edit/:userid',(req,res)=>{
     const userId = req.params.userid;
-    let sql = `select *from users where id=${userId}`;
-    let query = connection.query(sql, (err,result)=>{
+    let sql = `select * from reviewboard where id=${userId}`;
+    let query = dbConnection.query(sql, (err,result)=>{
         if(err) throw err;
         res.render('user_edit',{
             title:'게시글 편집',
@@ -66,21 +64,22 @@ app.get('/edit/:userid',(req,res)=>{
 
 app.post('/update', (req, res) => {
     const userId = req.body.id;
-    let sql = "update users SET name='" + req.body.name + "', title='" + req.body.title + "', message='" + req.body.message +"' where id="+userId;
-    let query = connection.query(sql, (err, results) => {
+    let sql = "update reviewboard SET name='" + req.body.name + "', title='" + req.body.title +"', message='" + req.body.message +"' where id="+userId;
+    let query = dbConnection.query(sql, (err, results) => {
         if (err) throw err;
-        res.redirect('/');
+        res.redirect('/review');
     });
 });
 
 app.get('/delete/:userid', (req, res) => {
     const userId = req.params.userid;
-    let sql = `DELETE from users where id=${userId}`;
-    let query = connection.query(sql, (err, result) => {
+    let sql = `DELETE from reviewboard where id=${userId}`;
+    let query = dbConnection.query(sql, (err, result) => {
         if (err) throw err;
-        res.redirect('/');
+        res.redirect('/review');
     });
 });
+
 
 
 //Server Listening
